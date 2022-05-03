66e0addade830aaa606fcc165b0d7f490b192747
KyuHyeong <rbgud2380@naver.com>
2022-05-03 23:40:35 +0900

    May 3rd Commit__

---
+++ src/COTE_220503/COTE_SOSU.java
@@ -0,0 +1,17 @@
+package COTE_220503;
+
+public class COTE_SOSU {
+
+}
+class Solution4 {
+	public int solution(int[] nums) {
+		int result = 0;
+		
+		for(int i : nums) {
+			System.out.println(i);
+		}
+		
+		
+		return result;
+	}
+}
