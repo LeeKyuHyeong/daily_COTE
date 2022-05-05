b5e19fcc27bcdf9aaf3c10b2167f5e0f1eb0bbce
KyuHyeong <rbgud2380@naver.com>
2022-05-06 00:00:02 +0900

    May 5th Commit_

--- src/COTE_220504/COTE_ANOTHER.java
+++ src/COTE_220504/COTE_ANOTHER.java
@@ -1,10 +1,20 @@
 package COTE_220504;
 
+import java.util.Scanner;
+
 public class COTE_ANOTHER {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		int i = sc.nextInt(); 
+		
+		switch(i) {
+		case 1:System.out.println(1);
+		case 2:System.out.println(2);
+		case 3:System.out.println(3);
+		}
 		
 	}
 }
