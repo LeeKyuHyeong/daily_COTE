754037055a2e227d4b277c5489533006a405d616
KyuHyeong <rbgud2380@naver.com>
2022-05-08 20:19:50 +0900

    May 8th Commit__

--- src/COTE_220507/COTE_ANOTHER.java
+++ src/COTE_220507/COTE_ANOTHER.java
@@ -5,11 +5,12 @@ public class COTE_ANOTHER {
 }
 class Solution1 {
 	public int solution(String s) {
-		String num[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		//String num[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 		String str[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 		
-		for(int i = 0; i < num.length; i++) {
-			s = s.replaceAll(str[i], num[i]);
+		for(int i = 0; i < str.length; i++) {
+			//s = s.replaceAll(str[i], num[i]);
+			s = s.replaceAll(str[i], Integer.toString(i));
 		}
 		
 		return Integer.parseInt(s);
