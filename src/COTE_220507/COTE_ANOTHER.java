71daa1f68dc3891368a4a4fb37660edc50f1cd33
KyuHyeong <rbgud2380@naver.com>
2022-05-08 19:07:30 +0900

    May 8th Commit_

--- src/COTE_220507/COTE_ANOTHER.java
+++ src/COTE_220507/COTE_ANOTHER.java
@@ -5,8 +5,6 @@ public class COTE_ANOTHER {
 }
 class Solution1 {
 	public int solution(String s) {
-		int answer = 0; 
-		int len = s.length();
 		String num[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 		String str[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 		
