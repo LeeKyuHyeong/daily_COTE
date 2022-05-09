f26b6fafcbf7821a19ce000ad6ec1cae188d2b9d
KyuHyeong <rbgud2380@naver.com>
2022-05-10 06:54:31 +0900

    May 10th Commit

--- src/COTE_220503/COTE_SOSU.java
+++ src/COTE_220503/COTE_SOSU.java
@@ -1,20 +1,30 @@
 package COTE_220503;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class COTE_SOSU {
 
 }
 class Solution4 {
 	public int solution(int[] nums) {
 		int result = 0;
-		int[] sumArr = new int[3];
+		List<Integer> intList = new ArrayList<Integer>();
 		
+		int sum = 0;
 		
 		for(int i = 0; i < nums.length; i++) {
-			sumArr[i] = nums[i];
-			if(sumArr.length == 3) {
+			intList.add(nums[i]);
+			if(intList.size() == 3) {
+				for(int j : intList) {
+					sum += j;
+					if(sum % 2 != 0) {
+						
+					}
+				}
+				intList.clear();
 			}
 		}
-		int sum = 0;
 		
 		
 		
