38f1d05b98ee116c5fdf25f7c1dcaa3593465d83
KyuHyeong <rbgud2380@naver.com>
2022-05-09 23:52:27 +0900

    May 9th Commit__

--- src/COTE_220503/COTE_SOSU.java
+++ src/COTE_220503/COTE_SOSU.java
@@ -6,12 +6,18 @@ public class COTE_SOSU {
 class Solution4 {
 	public int solution(int[] nums) {
 		int result = 0;
+		int[] sumArr = new int[3];
 		
 		
-		
-		for(int i : nums) {
-			System.out.println(i);
+		for(int i = 0; i < nums.length; i++) {
+			sumArr[i] = nums[i];
+			if(sumArr.length == 3) {
+			}
 		}
+		int sum = 0;
+		
+		
+		
 		
 		
 		return result;
