6db3d691dc0ba0c8281b366b490538cd8f5dd63e
KH <rbgud2380@naver.com>
2022-05-11 14:06:32 +0900

    May 11th Commit

---
+++ src/COTE_220511/COTE_TWOforONE.java
@@ -0,0 +1,33 @@
+package COTE_220511;
+import java.util.TreeSet;
+import java.util.Iterator;
+
+public class COTE_TWOforONE {
+    public static void main(String[] args) {
+        int numbers[] = {2,1,3,4,1};
+        Solution s = new Solution();
+
+        System.out.println(s.solution(numbers));
+    }
+}
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int [] answer;
+        TreeSet<Integer> intSet = new TreeSet<Integer>();
+
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i+1; j < numbers.length; i++) {
+                intSet.add(numbers[i] + numbers[j]);
+            }
+        }
+        Iterator iter = intSet.iterator();
+        answer = new int[intSet.size()];
+        while(iter.hasNext()){
+            for(int i = 0; i < intSet.size(); i++) {
+                answer[i] = Integer.parseInt(iter.next().toString());
+            }
+        }
+        return answer;
+    }
+}
