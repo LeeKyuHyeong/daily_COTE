4a0268c5922d17811ed419b4db34fb08958b5bb4
이규형 <rbgud2380@naver.com>
2023-01-01 17:24:43 +0900

    221231 paper cut

---
+++ src/COTE_221231/COTE_PRAC_pgs.java
@@ -0,0 +1,17 @@
+package COTE_221231;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+		
+		// 큰 종이를 1 X 1로 나누는 작업
+		// 2 X 2면 가로한번 세로두번(반대도 가능) 해서 3
+		
+		int N = 2;
+		int M = 2;
+		
+		int answer = N * M - 1;
+
+	}
+
+}
