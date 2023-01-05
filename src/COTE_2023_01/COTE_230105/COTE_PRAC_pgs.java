c3ca51b4229224f89afb69cd4a0929057df75cdc
이규형 <rbgud2380@naver.com>
2023-01-05 23:50:13 +0900

    230105 use for문

---
+++ src/COTE_2023_01/COTE_230105/COTE_PRAC_pgs.java
@@ -0,0 +1,17 @@
+package COTE_2023_01.COTE_230105;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+		
+		int n = 2;
+		int t = 10;
+		
+		for(int i = 0; i < t; i++) {
+			n *= 2;
+		}
+		System.out.println(n);
+
+	}
+
+}
