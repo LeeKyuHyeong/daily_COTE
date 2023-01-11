741af3a90c4b9d5582fbe4dcc68c9306d1c765da
이규형 <rbgud2380@naver.com>
2023-01-11 22:54:12 +0900

    230106 use Conditional operator

---
+++ src/COTE_2023_01/COTE_230106/COTE_PRAC_pgs.java
@@ -0,0 +1,17 @@
+package COTE_2023_01.COTE_230106;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+		
+		int n = 144;
+		
+		int answer = 0;
+		
+		answer = ((Math.sqrt(n) % 1) == 0.0) ? 1 : 2;
+	
+		System.out.println(answer);
+		
+	}
+
+}
