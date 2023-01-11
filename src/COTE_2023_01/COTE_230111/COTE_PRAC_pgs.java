4f8b29b5a0cde1e9e58c765643baeaedde8c2ff7
이규형 <rbgud2380@naver.com>
2023-01-12 00:01:57 +0900

    230111 use division

---
+++ src/COTE_2023_01/COTE_230111/COTE_PRAC_pgs.java
@@ -0,0 +1,14 @@
+package COTE_2023_01.COTE_230111;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+
+		int num1 = 4;
+		int num2 = 3;
+		
+		System.out.print(num1 / num2);
+		
+	}
+
+}
