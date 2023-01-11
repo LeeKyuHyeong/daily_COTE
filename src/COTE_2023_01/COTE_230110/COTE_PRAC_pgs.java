3e4e38fc1aafbe1aef86b45be0a62665a27f15c3
이규형 <rbgud2380@naver.com>
2023-01-11 22:58:38 +0900

    230110 use multiply

---
+++ src/COTE_2023_01/COTE_230110/COTE_PRAC_pgs.java
@@ -0,0 +1,14 @@
+package COTE_2023_01.COTE_230110;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+
+		int num1 = 2;
+		int num2 = 1;
+		
+		System.out.println(num1 * num2);
+		
+	}
+
+}
