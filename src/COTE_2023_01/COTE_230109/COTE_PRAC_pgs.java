a983d51ca9b68b7987afb4e91e90d48b6b9e288c
이규형 <rbgud2380@naver.com>
2023-01-11 22:55:56 +0900

    230109 use minus

---
+++ src/COTE_2023_01/COTE_230109/COTE_PRAC_pgs.java
@@ -0,0 +1,14 @@
+package COTE_2023_01.COTE_230109;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+
+		int num1 = 0;
+		int num2 = 0;
+		
+		System.out.println(num1 - num2);
+		
+	}
+
+}
