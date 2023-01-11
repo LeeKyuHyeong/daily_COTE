8dea30e5627e9ac914ef0e729c4e65f603517e74
이규형 <rbgud2380@naver.com>
2023-01-12 00:09:39 +0900

    230112 use rest

---
+++ src/COTE_2023_01/COTE_230112/COTE_PRAC_pgs.java
@@ -0,0 +1,14 @@
+package COTE_2023_01.COTE_230112;
+
+public class COTE_PRAC_pgs {
+
+	public static void main(String[] args) {
+
+		int num1 = 4;
+		int num2 = 3;
+		
+		System.out.println(num1 % num2);
+		
+	}
+
+}
