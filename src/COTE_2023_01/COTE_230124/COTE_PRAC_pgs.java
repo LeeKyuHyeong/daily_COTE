2a687a9a7319abcf82c11c8a7abf1987bcd45bdf
이규형 <rbgud2380@naver.com>
2023-01-25 23:49:35 +0900

    use triple if

---
+++ src/COTE_2023_01/COTE_230124/COTE_PRAC_pgs.java
@@ -0,0 +1,12 @@
+package COTE_2023_01.COTE_230124;
+
+public class COTE_PRAC_pgs {
+	
+	public static void main(String[] args) {
+		
+		int n = 7;
+		
+		System.out.println(n % 7 == 0 ? n / 7 : n / 7 + 1);
+		
+	}
+}
