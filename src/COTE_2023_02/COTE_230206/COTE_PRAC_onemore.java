1b0adbc458f374110664b87d33b6ef13d9551ccd
이규형 <rbgud2380@naver.com>
2023-02-07 00:24:06 +0900

    230206 use for

--- src/COTE_2023_02/COTE_230206/COTE_PRAC_onemore.java
+++ src/COTE_2023_02/COTE_230206/COTE_PRAC_onemore.java
@@ -4,7 +4,15 @@ public class COTE_PRAC_onemore {
 
 	public static void main(String[] args) {
 
+		int[] box = {10, 8, 6};
+		int n = 3;
 		
+		
+		int answer = box[0] / n;
+        
+        for(int i = 1; i < box.length; i++) {
+            answer *= (box[i] / n);
+        }
 		
 	}
 
