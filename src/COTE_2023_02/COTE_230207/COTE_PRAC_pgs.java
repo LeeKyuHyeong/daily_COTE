21e09aae9165ab6dac2e9d9665f53aa00bc8ae2c
이규형 <rbgud2380@naver.com>
2023-02-08 22:23:12 +0900

    230207 use set

--- src/COTE_2023_02/COTE_230207/COTE_PRAC_pgs.java
+++ src/COTE_2023_02/COTE_230207/COTE_PRAC_pgs.java
@@ -1,10 +1,25 @@
 package COTE_2023_02.COTE_230207;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class COTE_PRAC_pgs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Set<Integer> set = new TreeSet<>();
+		
+		int n = 1;
+		if(n == 1) {
+            set.add(1);
+        } else {
+			for(int i = 1; i < n / 2; i++) {
+				if(n % i == 0) {
+					set.add(i);
+					set.add(n / i);
+				}
+			}
+        }
+		System.out.println(set);
 	}
 
 }
