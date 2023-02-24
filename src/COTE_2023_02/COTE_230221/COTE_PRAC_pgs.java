a8815c9d1aae6f6257d0aeabd87ee9e23f94b900
leekh5 <rbgud2380@naver.com>
2023-02-24 16:56:29 +0900

    use factorial method

--- src/COTE_2023_02/COTE_230221/COTE_PRAC_pgs.java
+++ src/COTE_2023_02/COTE_230221/COTE_PRAC_pgs.java
@@ -4,6 +4,22 @@ public class COTE_PRAC_pgs {
 
 	public static void main(String[] args) {
 
+		int n = 119;
+		long result = 0L;
+		for(int i = 1; i <= 10; i++) {
+			if(fact(i) == n) {
+				result = i;
+			} else {
+				if(fact(i) < n)
+					result = i;
+			}
+		}
+		
+		System.out.println(result);
+		
 	}
-
+	static long fact(int n) {
+		
+		return n == 0 ? 1 : n * fact(n-1);
+	}
 }
