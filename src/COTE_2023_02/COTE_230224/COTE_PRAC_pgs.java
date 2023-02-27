d8bcbe14981a078064d5351e712cc8dcb6f554c8
leekh5 <rbgud2380@naver.com>
2023-02-27 15:14:16 +0900

    230227 use StringBuilder

--- src/COTE_2023_02/COTE_230224/COTE_PRAC_pgs.java
+++ src/COTE_2023_02/COTE_230224/COTE_PRAC_pgs.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class COTE_PRAC_pgs {
 
 	public static void main(String[] args) {
-		String str = "1a2b30c4d123Z";
+		String str = "a32";
 
 		StringBuilder sb = new StringBuilder();
 		
@@ -15,15 +15,18 @@ public class COTE_PRAC_pgs {
 		
 		for(String s : str.split("")) {
 			if(Pattern.matches(patternInt, s)) {
+				
 				sb.append(s);
+				System.out.println(sb.toString());
 			} else {
 				result += sb.toString().equals("") ? 0 : Integer.parseInt(sb.toString());
 				sb.delete(0, sb.length());
+				System.out.println(result);
 			}
 			
 		}
-		
-		System.out.println(result);
+		result += sb.toString().equals("") ? 0 : Integer.parseInt(sb.toString());
+		System.out.println("최종 : " + result);
 		
 	}
 
