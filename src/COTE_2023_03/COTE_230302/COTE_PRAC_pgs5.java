c3e751ff927b82e60f2bd574931f4588d715179d
leekh5 <rbgud2380@naver.com>
2023-03-02 17:59:18 +0900

    230302 use split not yet

---
+++ src/COTE_2023_03/COTE_230302/COTE_PRAC_pgs5.java
@@ -0,0 +1,30 @@
+package COTE_2023_03.COTE_230302;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class COTE_PRAC_pgs5 {
+	public static void main(String[] args) {
+		
+		String polynomial = "3x + 7 + x";
+		StringBuilder sb = new StringBuilder();
+		
+		Map<String, String> map = new HashMap<>();
+		
+		
+		//String[] strArr = polynomial.split(" + ");
+		
+		List<String> strList = new ArrayList<>();
+		
+		for(String s : polynomial.split("+")) {
+			strList.add(s);
+		}
+		
+		System.out.println(strList);
+		//System.out.println(Arrays.toString(strArr));
+		
+		
+	}
+}
