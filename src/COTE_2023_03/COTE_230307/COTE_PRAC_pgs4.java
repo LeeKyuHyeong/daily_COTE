7ffb98e6ea24d3d3bf46257b4760de8102768161
leekh5 <rbgud2380@naver.com>
2023-03-08 08:48:27 +0900

    230307 use char to int, int to char

--- src/COTE_2023_03/COTE_230307/COTE_PRAC_pgs4.java
+++ src/COTE_2023_03/COTE_230307/COTE_PRAC_pgs4.java
@@ -1,32 +1,32 @@
 package COTE_2023_03.COTE_230307;
 
-import java.util.Arrays;
-
 public class COTE_PRAC_pgs4 {
 
 	public static void main(String[] args) {
 
-		String str = "Z B z";
-		int n = 25;
+		String str = "a B z";
+		int n = 4;
 		char[] chArr = str.toCharArray();
 		
 		for(int i = 0; i < chArr.length; i++) {
 			if(chArr[i] == ' ') {
 				continue;
 			}
-			System.out.println(chArr[i]);
-			if(chArr[i] == 'z' || chArr[i] == 'Z') {
-				chArr[i] = (char)((int)chArr[i] - 26);
+			for(int j = 1; j < n; j++) {
+				
+				if(chArr[i] == 'z' || chArr[i] == 'Z') {
+					chArr[i] = (char)((int)chArr[i] - 26);
+				} else {
+					chArr[i] = (char)((int)chArr[i] + 1);
+				}
 			}
-			System.out.println(chArr[i]);
-			chArr[i] = (char)((int)chArr[i] + 1);
+			
 			System.out.println(chArr[i]);
 		}
-		//System.out.println(Arrays.toString(chArr));
 		
 		String answer = new String(chArr);
 		
-		//System.out.println(answer);
+		System.out.println(answer);
 	}
 
 }
