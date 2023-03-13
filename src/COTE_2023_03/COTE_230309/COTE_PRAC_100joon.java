a09ce9375f6dd5a916e45c3e4b802d010f6f94e2
이규형 <rbgud2380@naver.com>
2023-03-13 22:44:46 +0900

    230313 use flag for

--- src/COTE_2023_03/COTE_230309/COTE_PRAC_100joon.java
+++ src/COTE_2023_03/COTE_230309/COTE_PRAC_100joon.java
@@ -1,9 +1,9 @@
-package COTE_2023_03;
+package COTE_2023_03.COTE_230309;
 
 class COTE_PRAC_100joon {
     public static void main(String[] args) {
 
-        int num = 4;
+        int num = 16;
 
         String str = "";
         for(int i = 0; i < (num / 4); i++) {
