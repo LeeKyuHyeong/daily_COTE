9728381243d2b663714c9bc83924c0c4d44c2cd1
이규형 <rbgud2380@naver.com>
2023-05-21 10:00:31 +0900

    230521 commit

--- src/baekjoon/io/bj_2745.java
+++ src/baekjoon/io/bj_2745.java
@@ -1,8 +1,24 @@
 package baekjoon.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class bj_2745 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input = br.readLine();
+		
+		String number = input.split(" ")[0];
+		String arith = input.split(" ")[1];
+		
+		Long answer = 0L;
 		
 		
 	}
