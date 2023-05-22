545e9b20392e1d31f95fee2458146c7193ce0961
이규형 <rbgud2380@naver.com>
2023-05-22 21:17:13 +0900

    230522 commit

--- src/baekjoon/io/bj_2745.java
+++ src/baekjoon/io/bj_2745.java
@@ -5,6 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class bj_2745 {
 
@@ -16,10 +18,43 @@ public class bj_2745 {
 		String input = br.readLine();
 		
 		String number = input.split(" ")[0];
-		String arith = input.split(" ")[1];
+		int len = number.length();
 		
-		Long answer = 0L;
+		int arith = Integer.parseInt(input.split(" ")[1]);
 		
+		int answer = 0;
+		
+		String numReg = "^[0-9]*$";
+		
+		//int arith = 36;
+		if(arith > 10) {
+			
+			Map<Character, Integer> map = new HashMap<>(); 
+			
+			for(int i = 10; i < arith; i++) {
+				map.put((char)(55+i) , i);
+			}
+			
+			for(int j = 0; j < len; j++) {
+				
+				if(number.split("")[len-j-1].matches(numReg)) {
+					answer += Integer.parseInt(number.split("")[len-j-1]) * Math.pow(arith, j);
+				} else {
+					answer += map.get(number.charAt(len-j-1)) * Math.pow(arith, j);
+				}
+				
+				System.out.print(answer + "\t");
+			}
+			
+		} else {
+			for(int j = 0; j < len; j++) {
+				answer += Integer.parseInt(number.split("")[len-j-1]) * Math.pow(arith, j);
+				System.out.print(answer + "\t");
+			}
+			
+		}
+		
+		System.out.println(answer);
 		
 	}
 	
