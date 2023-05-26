b643449b1fa9703eb5925248f51a3984ade3f6dd
이규형 <rbgud2380@naver.com>
2023-05-26 22:59:35 +0900

    230523 commit not yet

--- src/COTE_2023_05/COTE_230523/COTE_PRAC_pgs.java
+++ src/COTE_2023_05/COTE_230523/COTE_PRAC_pgs.java
@@ -3,7 +3,15 @@ package COTE_2023_05.COTE_230523;
 public class COTE_PRAC_pgs {
 
 	public static void main(String[] args) {
-		
+		int[][] targets = {
+				{4, 5},
+				{4, 8},
+				{10, 14},
+				{11, 13},
+				{5, 12},
+				{3, 7},
+				{1, 4}
+		};
 	}
 	
 }
