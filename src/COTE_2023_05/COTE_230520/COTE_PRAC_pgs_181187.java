51c2c2fd7f6c886a7c8f5834b89d22b7f7d0ed37
이규형 <rbgud2380@naver.com>
2023-05-29 10:51:24 +0900

    230529 commit

--- src/COTE_2023_05/COTE_230520/COTE_PRAC_pgs_181187.java
+++ src/COTE_2023_05/COTE_230520/COTE_PRAC_pgs_181187.java
@@ -4,7 +4,7 @@ public class COTE_PRAC_pgs_181187 {
 
 	public static void main(String[] args) {
 		
-		int a = 4, b = 5;
+		int a = 999999, b = 1000000;
 				
 		System.out.println(solution(a, b));
 		
@@ -12,30 +12,34 @@ public class COTE_PRAC_pgs_181187 {
 	}
 	static long solution(int r1, int r2) {
         long answer = 0;
-        
-        answer += ((r2 - r1) + 1) * 4;
-        
-        long w1 = (long)Math.pow(r1, 2);
-        long w2 = (long)Math.pow(r2, 2);
-        
-        long cnt = 0;
-        
-        for(int i = 1; i < r2; i++) {
+        try {
+        	answer += ((r2 - r1) + 1) * 4;
         	
-        	for(int j = 1; j < r2; j++) {
-        		if( (long)Math.pow(i,2) + (long)Math.pow(j,2) >= w1 && (long)Math.pow(i,2) + (long)Math.pow(j,2) <= w2) {
-        			System.out.println("사잇값");
-        			System.out.print("i : " + i + "\t");
-        			System.out.print("j : " + j + "\t");
-        			cnt++;
+        	long w1 = (long)Math.pow(r1, 2);
+        	long w2 = (long)Math.pow(r2, 2);
+        	
+        	long cnt = 0;
+        	
+        	for(int i = 1; i < r2; i++) {
+        		
+        		for(int j = 1; j < r2; j++) {
+        			if( (long)Math.pow(i,2) + (long)Math.pow(j,2) >= w1 && (long)Math.pow(i,2) + (long)Math.pow(j,2) <= w2) {
+        				cnt++;
+        			}
         		}
         	}
-        	System.out.println("cnt : " + cnt);
+        	
+        	cnt *= 4;
+        	
+        	answer += cnt;
+        	
+        } catch(RuntimeException e) {
+        	e.printStackTrace();
+        } catch(StackOverflowError e) {
+        	e.printStackTrace();
+        } finally {
+        	System.out.println(answer);
         }
-        
-        cnt *= 4;
-        
-        answer += cnt;
         
         return answer;
     }
