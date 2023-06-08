acd46892101da09a56bc6ce443e91fb1c6abb85e
이규형 <rbgud2380@naver.com>
2023-06-08 21:57:22 +0900

    230608 dp codingTest

--- src/COTE_2023_06/COTE_20230606/COTE_PRAC_pgs.java
+++ src/COTE_2023_06/COTE_20230606/COTE_PRAC_pgs.java
@@ -1,8 +1,43 @@
 package COTE_2023_06.COTE_20230606;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class COTE_PRAC_pgs {
 	
-	public static void main(String[] args) {
+	static Integer[] dp;
+	
+	public static void main(String[] args) throws IOException{
 		// 1463 제출
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		dp = new Integer[n + 1000000];
+		dp[0] = dp[1] = 0;
+		
+		System.out.println(makeOne(n));
+		
+	}
+	static int makeOne(int n) {
+		
+		if(dp[n] == null) {
+			if(n % 6 == 0) {
+				dp[n] = Math.min(makeOne(n - 1), Math.min(makeOne(n / 3), makeOne(n / 2))) + 1;
+			}
+			else if(n % 3 ==0) {
+				dp[n] = Math.min(makeOne(n / 3), makeOne(n - 1)) + 1;
+			}
+			else if(n % 2 == 0) {
+				dp[n] = Math.min(makeOne(n / 2), makeOne(n - 1)) + 1;
+			}
+			else {
+				dp[n] = makeOne(n - 1) + 1;
+			}
+		}
+		
+		return dp[n];
 	}
 }
