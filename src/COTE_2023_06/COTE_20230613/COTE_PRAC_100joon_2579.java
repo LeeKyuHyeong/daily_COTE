eccc32eb76475a2d0e307c2d5a1d06ff37dc7e9a
이규형 <rbgud2380@naver.com>
2023-06-13 00:34:01 +0900

    230613 commit not yet

---
+++ src/COTE_2023_06/COTE_20230613/COTE_PRAC_100joon_2579.java
@@ -0,0 +1,16 @@
+package COTE_2023_06.COTE_20230613;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class COTE_PRAC_100joon_2579 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+	
+	
+}
