36bc85c7485d6a7146bb14dee7a0852a4c8de216
이규형 <rbgud2380@naver.com>
2023-06-15 00:10:11 +0900

    230615 commit

--- src/COTE_2023_06/COTE_20230614/COTE_PRAC_100joon_11053.java
+++ src/COTE_2023_06/COTE_20230614/COTE_PRAC_100joon_11053.java
@@ -3,7 +3,9 @@ package COTE_2023_06.COTE_20230614;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class COTE_PRAC_100joon_11053 {
@@ -22,8 +24,18 @@ public class COTE_PRAC_100joon_11053 {
 			arr[i] = Integer.parseInt(num.split(" ")[i]);
 		}
 		
-		
 		System.out.println(Arrays.toString(arr));
+		
+		
+		List<Integer> list = new ArrayList<>();
+		list.add(arr[0]);
+		
+		for(int i = 0; i < n-1; i++) {
+			if(arr[i] < arr[i+1]) {
+				list.add(arr[i+1]);
+			}
+		}
+		System.out.println(list);
 	}
 	
 }
