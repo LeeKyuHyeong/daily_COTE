8298b14840ae0dd18b7dde0e6dfd3b02245f1b1f
이규형 <rbgud2380@naver.com>
2023-06-19 06:18:35 +0900

    230619 commit

--- src/COTE_2023_06/COTE_20230614/COTE_PRAC_100joon_11053.java
+++ src/COTE_2023_06/COTE_20230614/COTE_PRAC_100joon_11053.java
@@ -10,32 +10,47 @@ import java.util.List;
 
 public class COTE_PRAC_100joon_11053 {
 	
+	static int[] seq;
+	static Integer[] dp;
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[n];
+		seq = new int[n];
+		dp = new Integer[n];
 		
 		String num = br.readLine();
 		
 		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(num.split(" ")[i]);
+			seq[i] = Integer.parseInt(num.split(" ")[i]);
 		}
 		
-		System.out.println(Arrays.toString(arr));
-		
-		
-		List<Integer> list = new ArrayList<>();
-		list.add(arr[0]);
-		
-		for(int i = 0; i < n-1; i++) {
-			if(arr[i] < arr[i+1]) {
-				list.add(arr[i+1]);
-			}
+		for(int i = 0; i < n; i++) {
+			LIS(n);
 		}
-		System.out.println(list.size());
+		
+		int max = dp[0];
+		
+		for(int i = 0; i < n; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+		
 	}
 	
+	static int LIS(int n) {
+		if(dp[n] == null) {
+			dp[n] = 1; 
+			
+			for(int i = n - 1; i >= 0; i--) {
+				if(seq[i] > seq[n]) {
+					dp[n] = Math.max(dp[n], LIS(i) + 1);
+				}
+			}
+		}
+		return dp[n];
+	}
 }
