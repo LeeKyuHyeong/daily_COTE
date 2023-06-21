8310904a81706a02235a891f8a1522026ee6b851
이규형 <rbgud2380@naver.com>
2023-06-22 00:02:39 +0900

    230622 commit

--- src/COTE_2023_06/COTE_20230619/COTE_PRAC_100joon_2720.java
+++ src/COTE_2023_06/COTE_20230619/COTE_PRAC_100joon_2720.java
@@ -3,7 +3,6 @@ package COTE_2023_06.COTE_20230619;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class COTE_PRAC_100joon_2720 {
 
@@ -30,7 +29,12 @@ public class COTE_PRAC_100joon_2720 {
 			tmp++;
 		}
 		
-		System.out.println(Arrays.deepToString(answer));
+		for(int i = 0; i < answer.length; i++) {
+			for(int j = 0; j < answer[i].length; j++) {
+				System.out.print(answer[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 	
 }
---
+++ src/COTE_2023_06/COTE_20230621/COTE_PRAC_100joon_2903.java
@@ -0,0 +1,11 @@
+package COTE_2023_06.COTE_20230621;
+
+public class COTE_PRAC_100joon_2903 {
+	
+	public static void main(String[] args) {
+		
+		//중앙 이동 알고리즘
+		
+	}
+	
+}
