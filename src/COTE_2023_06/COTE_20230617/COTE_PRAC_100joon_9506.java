ee768717cda62c3d43b2c6857d52e7ab3dcc89d5
이규형 <rbgud2380@naver.com>
2023-06-23 00:18:07 +0900

    230623 100joon 9506 clear

---
+++ src/COTE_2023_06/COTE_20230617/COTE_PRAC_100joon_9506.java
@@ -0,0 +1,63 @@
+package COTE_2023_06.COTE_20230617;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class COTE_PRAC_100joon_9506 {
+
+	static Integer[] dp;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		boolean flag = true;
+		
+		int tmp = 0;
+		
+		Map<Integer, String> map = new HashMap<>();
+		
+		while(flag) {
+			
+			int n = sc.nextInt();
+			
+			if(n == -1) {
+				flag = false;
+				break;
+			}
+			
+			map.put(n, perfect(n));
+			
+			tmp++;
+		}
+
+		for(int i : map.keySet()) {
+			System.out.println(map.get(i));
+		}
+	}
+	
+	static String perfect(int n) {
+		
+		String result = n + " ";
+		
+		int sum = 1;
+		
+		String perfStr = "= 1";
+		String notPerfStr = "is NOT perfect.";
+		
+		for(int i = 2; i <= n / 2; i++) {
+			if(n % i == 0) {
+				sum += i;
+				perfStr += " + " + i;
+			}
+		}
+		
+		result += sum == n ? perfStr : notPerfStr;
+		
+		return result;
+	}
+	
+	
+	
+}
