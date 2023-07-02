a35d16124c52c8f17490aa108934cc57366f505e
이규형 <rbgud2380@naver.com>
2023-07-03 00:09:42 +0900

    230703 commit

--- src/COTE_2023_06/COTE_20230628/COTE_PRAC_100joon_27323.java
+++ src/COTE_2023_06/COTE_20230628/COTE_PRAC_100joon_27323.java
@@ -1,10 +1,17 @@
 package COTE_2023_06.COTE_20230628;
 
+import java.util.Scanner;
+
 public class COTE_PRAC_100joon_27323 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A * B);
 		
 	}
 	
