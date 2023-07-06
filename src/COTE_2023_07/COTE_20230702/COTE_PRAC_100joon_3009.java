8b237e7bcf406a4c1669e0b467f4f24c0774e325
이규형 <rbgud2380@naver.com>
2023-07-07 00:26:28 +0900

    230707 commit

---
+++ src/COTE_2023_07/COTE_20230702/COTE_PRAC_100joon_3009.java
@@ -0,0 +1,31 @@
+package COTE_2023_07.COTE_20230702;
+
+public class COTE_PRAC_100joon_3009 {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * 												l
+		 * 												l
+		 * 												l          y    answer
+		 * 												l
+		 * 												l          x    z
+		 * 												l
+		 * 												l
+		 * 												l
+		  												l          		
+		  				----------------------------------------------------------------																			
+		  												l
+		  												l
+		  												l
+		  												l
+		  												l
+		  												l
+		  												l
+		  												l
+		  												l
+		 * 
+		 */
+		
+	}
+}
