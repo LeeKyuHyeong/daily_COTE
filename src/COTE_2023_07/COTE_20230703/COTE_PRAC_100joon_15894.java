3d446179b1558c1a30a7222e92914fff9162e3a9
이규형 <rbgud2380@naver.com>
2023-07-09 09:58:18 +0900

    20230709 commit

---
+++ src/COTE_2023_07/COTE_20230703/COTE_PRAC_100joon_15894.java
@@ -0,0 +1,17 @@
+package COTE_2023_07.COTE_20230703;
+
+import java.util.Scanner;
+
+public class COTE_PRAC_100joon_15894 {
+	public static void main(String[] args) {
+		/*
+		 * 1	4
+		 * 2	8
+		 * 3	12
+		 * 4	16
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextInt() * 4);
+	}
+}
