d49041cae7427f191106af6f6465757fcfd65861
이규형 <rbgud2380@naver.com>
2023-07-16 20:27:35 +0900

    230716 commit

--- src/COTE_2023_07/COTE_20230707/COTE_PRAC_100joon_14215.java
+++ src/COTE_2023_07/COTE_20230707/COTE_PRAC_100joon_14215.java
@@ -1,8 +1,16 @@
 package COTE_2023_07.COTE_20230707;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class COTE_PRAC_100joon_14215 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
 		
 	}
 }
