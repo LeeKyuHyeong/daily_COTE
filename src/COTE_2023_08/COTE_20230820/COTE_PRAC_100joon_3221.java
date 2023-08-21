bd02a1893ead82bd259f46160c0c147a627b5ad0
이규형 <rbgud2380@naver.com>
2023-08-21 22:37:49 +0900

    230821 commit

--- src/COTE_2023_08/COTE_20230820/COTE_PRAC_100joon_3221.java
+++ src/COTE_2023_08/COTE_20230820/COTE_PRAC_100joon_3221.java
@@ -1,5 +1,27 @@
 package COTE_2023_08.COTE_20230820;
 
-public class COTE_PRAC_100joon_3221 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class COTE_PRAC_100joon_3221 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String size = br.readLine();
+		
+		int x = Integer.parseInt(size.split(" ")[0]);
+		int y = Integer.parseInt(size.split(" ")[1]);
+		
+		char chess[][] = new char[x][y]; 
+		
+		
+		
+		for(int i = 0; i < chess.length; i++) {
+			
+		}
+		
+		
+	}
 }
