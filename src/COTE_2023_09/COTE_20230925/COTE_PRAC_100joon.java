ab0ec109bcbc57e1272b1a1561538f448bf6647b
이규형 <rbgud2380@naver.com>
2023-10-03 22:41:02 +0900

    231003 daily COTE

--- src/COTE_2023_09/COTE_20230925/COTE_PRAC_100joon.java
+++ src/COTE_2023_09/COTE_20230925/COTE_PRAC_100joon.java
@@ -1,7 +1,14 @@
 package COTE_2023_09.COTE_20230925;
 
+import java.util.Scanner;
+
 public class COTE_PRAC_100joon {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        System.out.println(A * B);
     }
 }
