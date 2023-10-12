b9554bfef78c3b6b88ee7cebb928e9b18a0d1c0e
이규형 <rbgud2380@naver.com>
2023-10-12 21:08:40 +0900

    231001 daily COTE

---
+++ src/COTE_2023_10/COTE_20231001/COTE_PRAC_100joon_5073.java
@@ -0,0 +1,51 @@
+package COTE_2023_10.COTE_20231001;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class COTE_PRAC_100joon_5073 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		boolean flag = true;
+		
+		int cnt = 1;
+		
+		Map<Integer, String> map = new HashMap<>();
+		
+		while(flag) {
+			String input = br.readLine();
+			
+			if(input.equals("0 0 0")) {
+				flag = false;
+				break;
+			}
+			
+			int a = Integer.parseInt(input.split(" ")[0]);
+			int b = Integer.parseInt(input.split(" ")[1]);
+			int c = Integer.parseInt(input.split(" ")[2]);
+			
+			map.put(cnt, (a == b && b == c) ? "Equilateral" : (a == b || b == c || c == a) ? "Isosceles" : ( (a+b+c) - Math.max(Math.max(a, b), c) > Math.max(Math.max(a, b), c) ) ? "Scalene" : "Invalid");
+			
+			cnt++;
+		}
+		
+		br.close();
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i = 1; i < cnt; i++) {
+			bw.write(map.get(i) + "\n");
+		}
+		bw.flush();
+		bw.close();
+		
+	}
+}
