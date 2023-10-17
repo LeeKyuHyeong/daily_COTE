913bfcb889d66b3044a9851c657ad024c9d05369
이규형 <rbgud2380@naver.com>
2023-10-17 21:19:51 +0900

    231004 daily COTE

---
+++ src/COTE_2023_10/COTE_20231004/COTE_PRAC_100joon_11651.java
@@ -0,0 +1,47 @@
+package COTE_2023_10.COTE_20231004;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class COTE_PRAC_100joon_11651 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String lengthStr = br.readLine();
+		int length = Integer.parseInt(lengthStr);
+
+		int[][] arr = new int[length][2];
+		int tmp = 0;
+
+		while (length > tmp) {
+			String inputStr = "";
+			inputStr = br.readLine();
+
+			arr[tmp][0] = Integer.parseInt(inputStr.split(" ")[0]);
+			arr[tmp][1] = Integer.parseInt(inputStr.split(" ")[1]);
+
+			tmp++;
+		}
+
+		Arrays.sort(arr, new Comparator<int[]>() {
+			public int compare(int[] arr1, int[] arr2) {
+				return arr1[1] != arr2[1] ? arr1[1] - arr2[1] : arr1[0] - arr2[0];
+			}
+		});
+
+		for (int i = 0; i < length; i++) {
+			bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
