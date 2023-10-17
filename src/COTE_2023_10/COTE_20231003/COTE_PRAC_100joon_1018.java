f7d6be7a9aa653eca9d1950083cf8c495d7c70e9
이규형 <rbgud2380@naver.com>
2023-10-17 21:15:16 +0900

    231017 commit

--- src/COTE_2023_10/COTE_20231003/COTE_PRAC_100joon_1018.java
+++ src/COTE_2023_10/COTE_20231003/COTE_PRAC_100joon_1018.java
@@ -3,133 +3,69 @@ package COTE_2023_10.COTE_20231003;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class COTE_PRAC_100joon_1018 {
+	
+	public static boolean[][] arr;
+	public static int MIN = 64;
+	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String tiles = br.readLine();
+		String str = br.readLine();
 		
-		int N = Integer.parseInt(tiles.split(" ")[0]);
-		int M = Integer.parseInt(tiles.split(" ")[1]);
+		int N = Integer.parseInt(str.split(" ")[0]);
+		int M = Integer.parseInt(str.split(" ")[1]);
 		
-		String strArr[][] = new String[N][M];
-		
-		System.out.println(Arrays.deepToString(strArr));
+		arr = new boolean[N][M];
 		
 		for(int i = 0; i < N; i++) {
 			String input = br.readLine();
+			
 			for(int j = 0; j < M; j++) {
-				strArr[i][j] = input.split("")[j];
-			}
-		}
-		System.out.println(Arrays.deepToString(strArr));
-		
-		int MIN = 64;
-		
-		//한줄 안
-		int cycle = 0;
-		for(int i = 0; i < N - 7; i++) {
-			//세로 한칸 아래
-			
-			for(int j = 0; j < M - 7; j++) {
-				//가로 한칸 오른쪽
-				
-				String[][] eachStrArr = new String[8][8];
-				for(int k = i; k < i + 8; k++) {
-					// 세로 반복
-					
-					for(int l = j; l < j + 8; l++) {
-						// 가로 반복
-						System.out.print("k : " + k + ", l : "+ l + ",strArr[k][l] : " + strArr[k][l] + "\t");
-						eachStrArr[k-i][l-j] = strArr[k][l];
-						
-					}
-					//가로반복 끝
+				if(input.charAt(j) == 'W') {
+					arr[i][j] = true;
+				} else {
+					arr[i][j] = false;
 				}
-				//세로반복 끝 (한개 8*8 완성)
-				cycle++;
-				System.out.print(cycle + "번 " + Arrays.deepToString(eachStrArr) + "\t");
-				//System.out.println(cycle + "번 " + minRepair(eachStrArr));
-				MIN = Math.min(minRepair(cycle, eachStrArr), MIN);
-				System.out.println(MIN);
 			}
-			
 		}
 		
-		/*
-		 BBWBWBWW
-		 BWBWBWBW
-		 WBWBWBWB
-		 
-		 
-		 
-		 
-		 WWBWBWBB
-		 WBWBWBWB
-		 */
+		int N_row = N -7;
+		int M_row = M -7;
 		
-		
-		
-		
+		for(int i = 0; i < N_row; i++) {
+			for(int j = 0; j < M_row; j++) {
+				find(i, j);
+			}
+		}
 		
 		System.out.println(MIN);
 		
 		
-		
-		
-		
 	}
-	public static int minRepair(int cycle, String[][] arr) {
+	
+	public static void find(int x, int y) {
+		int end_x = x + 8;
+		int end_y = y + 8;
+		int cnt = 0;
 		
-		int changeStart = 0;
-		int changeSecond = 0;
+		boolean TF = arr[x][y];
 		
-		boolean toggle = true;
-		
-		String startStr = arr[0][0];
-		
-		String otherStr = startStr.equals("W") ? "B" : "W";
-		
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = x; i < end_x; i++) {
 			
-			String tmp = "";
-			tmp = startStr;
-			startStr = otherStr;
-			otherStr = tmp;
-			
-			for(int j = 0; j < arr[i].length; j++) {
-				//System.out.println("i : " + i + ", j : "+ j + ",arr[i][j] : " + arr[i][j] + ",toggle : " + toggle);
+			for(int j = y; j < end_y; j++) {
 				
-				if(!toggle && arr[i][j].equals(startStr)) {
-					changeStart++;
-				} else if(toggle && arr[i][j].equals(otherStr)) {
-					changeStart++;
+				if(arr[i][j] != TF) {
+					cnt++;
 				}
-				
-				//toggle = !toggle;
+				TF = (!TF);
 			}
-			toggle = !toggle;
+			TF = !TF;
 		}
+		cnt = Math.min(cnt, 64 - cnt);
 		
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = 0; j < arr[i].length; j++) {
-				System.out.println("i : " + i + ", j : "+ j + ",arr[i][j] : " + arr[i][j] + ",toggle : " + toggle);
-				
-				if(!toggle && arr[i][j].equals(startStr)) {
-					changeSecond++;
-				} else if(toggle && arr[i][j].equals(otherStr)) {
-					changeSecond++;
-				}
-				
-				//toggle = !toggle;
-			}
-			toggle = !toggle;
-		}
-		
-		
-		return Math.min(changeStart, changeSecond);
+		MIN = Math.min(MIN, cnt);
 	}
 }
