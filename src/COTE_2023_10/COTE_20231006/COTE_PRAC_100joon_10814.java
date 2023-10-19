e5b7788759e0c0dd931f39bcbb3f71e4d381d55d
이규형 <rbgud2380@naver.com>
2023-10-20 00:25:40 +0900

    231006 daily COTE not yet

---
+++ src/COTE_2023_10/COTE_20231006/COTE_PRAC_100joon_10814.java
@@ -0,0 +1,40 @@
+package COTE_2023_10.COTE_20231006;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class COTE_PRAC_100joon_10814 {
+	
+	public static void main(String[] args) throws IOException{
+				
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int cycle = Integer.parseInt(br.readLine());
+		
+		List<Map<Integer, String>> list = new ArrayList<Map<Integer, String>>();
+		
+		
+		for(int i = 0; i < cycle; i++) {
+			
+			String input = br.readLine();
+			
+			//Integer.parseInt(input.split(" ")[0]), input.split(" ")[1]);
+			
+			//list.add(map);
+			
+		}
+
+		
+		
+		
+	}
+
+}
