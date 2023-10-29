78d63e1a8ee75f68b3cd3e12d79056bedd889b14
이규형 <rbgud2380@naver.com>
2023-10-29 21:59:22 +0900

    231029 commit

--- src/COTE_2023_10/COTE_20231006/COTE_PRAC_100joon_10814.java
+++ src/COTE_2023_10/COTE_20231006/COTE_PRAC_100joon_10814.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,16 +28,14 @@ public class COTE_PRAC_100joon_10814 {
 			
 			String input = br.readLine();
 			
-			
 			param.put(Integer.parseInt(input.split(" ")[0]), input.split(" ")[1]);
 			
 			list.add(param);
 		}
-
-		System.out.println("야근해도아무도안알아준다!");
 		
-		System.out.println(list);
-		
+		list.sort(new Comparator<String>(String str1, String str2) {
+			return str1 - str2;
+		});
 		
 	}
 
