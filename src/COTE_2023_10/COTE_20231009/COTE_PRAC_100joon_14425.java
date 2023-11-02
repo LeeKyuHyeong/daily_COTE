ba9fff1cfaa91794b27028869ad55da226f6dc51
이규형 <rbgud2380@naver.com>
2023-11-02 22:19:39 +0900

    231102 commit

---
+++ src/COTE_2023_10/COTE_20231009/COTE_PRAC_100joon_14425.java
@@ -0,0 +1,31 @@
+package COTE_2023_10.COTE_20231009;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class COTE_PRAC_100joon_14425 {
+	
+	static int arr2[];
+	static int arr1[];
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine().split(" ")[0]);
+		
+		int M = Integer.parseInt(br.readLine().split(" ")[1]);
+		
+		arr1 = new int[N];
+		
+		arr2 = new int[M];
+		
+		
+		
+	}
+}
