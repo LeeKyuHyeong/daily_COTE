e10604afdbb037856e26def11fb2b1a72218745d
이규형 <rbgud2380@naver.com>
2024-01-25 01:39:46 +0900

    240125 daily COTE

---
+++ src/COTE_2024_01/COTE_PRAC_100joon_2562.java
@@ -0,0 +1,80 @@
+package COTE_2024_01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class COTE_PRAC_100joon_2562 {
+
+	public static void main(String[] args) throws IOException{
+		
+		
+		
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        boolean flag = true;
+        
+        while(flag) {
+        	String input = br.readLine();
+        	
+        	if("0 0".equals(input)) {
+//        		flag = false;
+        		System.exit(0);
+        	}
+        	System.out.println(input);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        int MAX = -1;
+        int maxIdx = 0;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        for(int i = 0; i < 9; i++) {
+            String strInput = br.readLine();
+            int input = Integer.parseInt(strInput);
+            if(input > MAX){
+                MAX = input;
+                maxIdx = i;
+            }            
+        }
+        
+        br.close();
+        
+        String str = "ab";
+        
+        String ch = str.split("")[0];
+        
+        bw.write(String.valueOf(MAX) + "\n" + String.valueOf(maxIdx));
+        bw.flush();
+        bw.close();
+        
+	}
+}
