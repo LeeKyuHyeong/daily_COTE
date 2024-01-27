1fed0a5df1d5914d09e9c149a526e796517ff1a8
leegunha <gunha8313@naver.com>
2024-01-27 15:29:12 +0900

    240127 commit

--- src/COTE_220521/COTE_PRAC1.java
+++ src/COTE_220521/COTE_PRAC1.java
@@ -19,13 +19,7 @@ public class COTE_PRAC1 {
 				
 			}
 		}
-		System.out.println(" 건하커플은 건하가 좀 더 감수성이다. ");
 
-		System.out.println("하지만 이제는 혼자다");
-
-		System.out.println("하지만 다음주에 소개팅하러 간다! 새로운 만남!");
-
-		System.out.println("하지만 다음주에 소개팅하러 간다2! 새로운 만남2! 제발!");
 		
 	}
 }
