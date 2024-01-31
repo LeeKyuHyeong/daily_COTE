01284142e4005a7bc7973cebebe18802eba12d91
이규형 <rbgud2380@naver.com>
2024-02-01 08:00:22 +0900

    240201 daily COTE

--- src/COTE_2024_01/COTE_PRAC_100joon_1436.java
+++ src/COTE_2024_01/COTE_PRAC_100joon_1436.java
@@ -12,16 +12,17 @@ public class COTE_PRAC_100joon_1436 {
 		int size = sc.nextInt();
 		
 		intArr = new int[size];
-		
+		//처음세팅
 		intArr[0] = 666;
 		
 		int tmp = 1;
-		
+		//1666부터시작
 		int num = 1666;
 		
 		while(tmp < size) {
 			
 			if(String.valueOf(num).indexOf("666") > -1) {
+				//666을포함하면
 				intArr[tmp] = num;
 				
 				tmp++;
