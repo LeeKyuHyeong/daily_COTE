ad4bf9e0f2e80e9d4dd9339deeda8784b07862ef
이규형 <rbgud2380@naver.com>
2024-02-23 20:13:16 +0900

    240223 dailyCOTE

---
+++ src/COTE_2024_02/COTE_PRAC_100joon_2108.java
@@ -0,0 +1,38 @@
+package COTE_2024_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class COTE_PRAC_100joon_2108 {
+	
+	public static int[] arr;
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int cnt = Integer.parseInt(br.readLine());
+		
+		arr = new int[cnt];
+		
+		int sum = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(Arrays.toString(arr));
+		
+		System.out.println(sum / arr.length);
+		System.out.println(arr[arr.length / 2]);
+		
+		System.out.println(arr[arr.length - 1] - arr[0]);
+		
+		
+	}
+}
